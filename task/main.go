eba55a2539683658360bffe839c98110ac9a8838
Adrian Rutkowski <edwi_91@o2.pl>
2018-06-26 20:17:26 +0200

    cwiczenie 7

---
+++ task/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gophercises/task/cmd"
+	"github.com/gophercises/task/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
+
+func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+
+	fmt.Println("db stuff ruszyło!!")
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
